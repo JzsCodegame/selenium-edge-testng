b200ba4a8338ea110a70d3de6a8399cd3b3a9d99
Javid <jzsplayground@gmail.com>
2026-02-07 12:25:35 -0500

    Remove test-output directory from tracking

---
+++ src/test/java/com/example/tests/OpenEdgeTest.java
@@ -0,0 +1,31 @@
+package com.example.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class OpenEdgeTest {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();
+    }
+
+    @Test
+    public void openWebsite() {
+        driver.get("https://automationexercise.com/"); // replace with your site
+        System.out.println("Title: " + driver.getTitle());
+    }
+
+//    @AfterMethod(alwaysRun = true)
+//    public void tearDown() {
+//        if (driver != null) driver.quit();
+//    }
+}
+
